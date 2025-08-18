4b3426beb33444c481ee28b085f6cecfaadc95b8
Redstonneur1256 <redstonneur1256mc@gmail.com>
2025-08-18 11:16:32 +0200

    Fix terrain generation crash

--- src/main/java/com/lightning/northstar/world/features/CraterFeature.java
+++ src/main/java/com/lightning/northstar/world/features/CraterFeature.java
@@ -12,82 +12,78 @@ import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 
 public class CraterFeature extends Feature<CraterConfig> {
 
-    public CraterFeature(Codec<CraterConfig> pCodec) {
-        super(pCodec);
+    public CraterFeature(Codec<CraterConfig> codec) {
+        super(codec);
     }
 
     @Override
-    public boolean place(FeaturePlaceContext<CraterConfig> pContext) {
-          CraterConfig config = pContext.config();
-          BlockPos blockpos = pContext.origin();
-          WorldGenLevel worldgenlevel = pContext.level();
-          RandomSource randomsource = pContext.random();
-          boolean flag = false;
-          int i = blockpos.getY();
-          int j = i + config.half_height.sample(randomsource);
-          if(j == 0)
-              return false;
-          int k = i - config.half_height.sample(randomsource) - 1;
-          int l = config.radius.sample(randomsource);
-          BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
-          int pp = 0;
-          for(int l_old = l;l > l_old / j; l = (int) (l / 1.5)) {
-              if (pp < config.depth.sample(randomsource)) {
-                  pp++;
-                  blockpos = blockpos.below();
-              }
-              for(BlockPos blockpos1 : BlockPos.betweenClosed(blockpos.offset(-l, 0, -l), blockpos.offset(l, 0, l))) {
-                  int i1 = blockpos1.getX() - blockpos.getX();
-                  int j1 = blockpos1.getZ() - blockpos.getZ();
-                  if (i1 * i1 + j1 * j1 <= l * l - 0.1) {
-                      flag |= this.placeColumn(config, worldgenlevel, randomsource, j, k, blockpos$mutableblockpos.set(blockpos1));
-                      this.placeColumnBelow(config, worldgenlevel, randomsource, j, k, blockpos$mutableblockpos.set(blockpos1.below()));
-                      this.clearAir(config, worldgenlevel, randomsource, j, k, blockpos$mutableblockpos.set(blockpos1));
-                  }
-              }
-          }
+    public boolean place(FeaturePlaceContext<CraterConfig> context) {
+        CraterConfig config = context.config();
+        BlockPos center = context.origin();
+        WorldGenLevel level = context.level();
+        RandomSource random = context.random();
+        boolean flag = false;
+        int j = center.getY() + config.halfHeight().sample(random);
+        if (j <= 0) // lazy solution for now (prevent generation below Y 0), world gen rewrite is planned anyway
+            return false;
+        int k = center.getY() - config.halfHeight().sample(random) - 1;
+        int l = config.radius().sample(random);
+        BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
+        int pp = 0;
+        for (int l_old = l; l > l_old / j && j > 0; l = (int) (l / 1.5)) {
+            if (pp < config.depth().sample(random)) {
+                pp++;
+                center = center.below();
+            }
+            for (BlockPos blockpos1 : BlockPos.betweenClosed(center.offset(-l, 0, -l), center.offset(l, 0, l))) {
+                int i1 = blockpos1.getX() - center.getX();
+                int j1 = blockpos1.getZ() - center.getZ();
+                if (i1 * i1 + j1 * j1 <= l * l - 0.1) {
+                    flag |= this.placeColumn(config, level, random, j, k, blockpos$mutableblockpos.set(blockpos1));
+                    this.placeColumnBelow(config, level, random, j, k, blockpos$mutableblockpos.set(blockpos1.below()));
+                    this.clearAir(config, level, random, j, k, blockpos$mutableblockpos.set(blockpos1));
+                }
+            }
+        }
 
-          return flag;
+        return flag;
     }
 
-     protected boolean placeColumn(CraterConfig pConfig, WorldGenLevel pLevel, RandomSource pRandom, int pMaxY, int pMinY, BlockPos.MutableBlockPos pPos) {
-          boolean flag = false;
-          BlockState blockstate = pConfig.air_provider.getState(pRandom, pPos);
+    protected boolean placeColumn(CraterConfig pConfig, WorldGenLevel pLevel, RandomSource pRandom, int pMaxY, int pMinY, BlockPos.MutableBlockPos pPos) {
+        BlockState blockstate = pConfig.airProvider().getState(pRandom, pPos);
 
-          if(pLevel.getBlockState(pPos).is(pConfig.canDelete)) {
-              pLevel.setBlock(pPos, blockstate, 2);
-          }
-          this.markAboveForPostProcessing(pLevel, pPos);
-          flag = true;
+        if (pLevel.getBlockState(pPos).is(pConfig.canDelete())) {
+            pLevel.setBlock(pPos, blockstate, 2);
+        }
+        this.markAboveForPostProcessing(pLevel, pPos);
 
-          return flag;
-       }
-     protected boolean clearAir(CraterConfig pConfig, WorldGenLevel pLevel, RandomSource pRandom, int pMaxY, int pMinY, BlockPos.MutableBlockPos pPos) {
-          boolean flag = false;
-          BlockState blockstate = pConfig.air_provider.getState(pRandom, pPos);
-          for(int i = 0; i < 24; i++) {
-          if(pLevel.getBlockState(pPos.atY(pPos.getY() + i)).is(pConfig.canDelete)) {
-                  pLevel.setBlock(pPos.atY(pPos.getY() + i), blockstate, 2);
-          }
-          flag = true;}
+        return true;
+    }
+
+    protected boolean clearAir(CraterConfig pConfig, WorldGenLevel pLevel, RandomSource pRandom, int pMaxY, int pMinY, BlockPos.MutableBlockPos pPos) {
+        boolean flag = false;
+        BlockState blockstate = pConfig.airProvider().getState(pRandom, pPos);
+        for (int i = 0; i < 24; i++) {
+            if (pLevel.getBlockState(pPos.atY(pPos.getY() + i)).is(pConfig.canDelete())) {
+                pLevel.setBlock(pPos.atY(pPos.getY() + i), blockstate, 2);
+            }
+            flag = true;
+        }
+
+        return flag;
+    }
 
 
-          return flag;
-       }
+    protected boolean placeColumnBelow(CraterConfig pConfig, WorldGenLevel pLevel, RandomSource pRandom, int pMaxY, int pMinY, BlockPos.MutableBlockPos pPos) {
+        BlockState blockstate = pConfig.blockProvider().getState(pRandom, pPos);
+        if (pLevel.getBlockState(pPos) == Blocks.AIR.defaultBlockState()) {
+            return false;
+        }
+        if (pLevel.getBlockState(pPos).is(pConfig.canDelete())) {
+            pLevel.setBlock(pPos, blockstate, 2);
+        }
+        this.markAboveForPostProcessing(pLevel, pPos);
+        return true;
+    }
 
-
-     protected boolean placeColumnBelow(CraterConfig pConfig, WorldGenLevel pLevel, RandomSource pRandom, int pMaxY, int pMinY, BlockPos.MutableBlockPos pPos) {
-          boolean flag = false;
-          BlockState blockstate = pConfig.block_provider.getState(pRandom, pPos);
-          if (pLevel.getBlockState(pPos) == Blocks.AIR.defaultBlockState()) {
-              return false;
-          }
-          if(pLevel.getBlockState(pPos).is(pConfig.canDelete)) {
-              pLevel.setBlock(pPos, blockstate, 2);
-          }
-          this.markAboveForPostProcessing(pLevel, pPos);
-          flag = true;
-
-          return flag;
-       }
 }
