17fdc66653d6a4792ee9d84b42d8d0cd598e5402
Redstonneur1256 <redstonneur1256mc@gmail.com>
2025-09-28 23:25:28 +0200

    Small mixin fixes

--- src/main/java/com/lightning/northstar/mixin/create/ContraptionColliderMixin.java
+++ src/main/java/com/lightning/northstar/mixin/create/ContraptionColliderMixin.java
@@ -1,4 +1,4 @@
-package com.lightning.northstar.mixin;
+package com.lightning.northstar.mixin.create;
 
 import com.lightning.northstar.contraption.rocket.RocketContraptionEntity;
 import com.llamalad7.mixinextras.sugar.Local;
@@ -9,10 +9,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ContraptionCollider.class)
+@Mixin(value = ContraptionCollider.class, remap = false)
 public class ContraptionColliderMixin {
 
-    @Redirect(method = "collideEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/Vec3;subtract(Lnet/minecraft/world/phys/Vec3;)Lnet/minecraft/world/phys/Vec3;", ordinal = 0))
+    @Redirect(method = "collideEntities",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/phys/Vec3;subtract(Lnet/minecraft/world/phys/Vec3;)Lnet/minecraft/world/phys/Vec3;",
+                    ordinal = 0,
+                    remap = false))
     private static Vec3 northstar$changeBounds(Vec3 instance, Vec3 vec, @Local(argsOnly = true) AbstractContraptionEntity entity) {
         if (entity instanceof RocketContraptionEntity) {
             return Vec3.ZERO; // pretend that we haven't moved, entities are moved manually in RocketContraptionEntity
