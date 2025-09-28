78d0c90a46b453312d5dc7c1e41de57be6bd5b23
Redstonneur1256 <redstonneur1256mc@gmail.com>
2025-09-28 19:01:02 +0200

    Fix candles being lit on placement and cakes

--- src/main/java/com/lightning/northstar/mixin/block/CandleCakeBlockMixin.java
+++ src/main/java/com/lightning/northstar/mixin/block/CandleCakeBlockMixin.java
@@ -9,37 +9,34 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.AbstractCandleBlock;
-import net.minecraft.world.level.block.CandleBlock;
+import net.minecraft.world.level.block.CandleCakeBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(CandleBlock.class)
-public abstract class CandleBlockMixin extends AbstractCandleBlock {
+@Mixin(CandleCakeBlock.class)
+public abstract class CandleCakeBlockMixin extends AbstractCandleBlock {
 
-    protected CandleBlockMixin(Properties properties) {
+    protected CandleCakeBlockMixin(Properties properties) {
         super(properties);
     }
 
-    @Inject(method = "getStateForPlacement", at = @At("RETURN"), cancellable = true)
-    public void northstar$updatePlacementLit(BlockPlaceContext context, CallbackInfoReturnable<BlockState> info) {
-        BlockState state = info.getReturnValue() == null ? defaultBlockState() : info.getReturnValue();
-
-        if (!NorthstarOxygen.hasOxygen(context.getLevel(), context.getClickedPos())) {
-            state = state.setValue(LIT, false);
-        }
-
-        info.setReturnValue(state);
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState()
+                .setValue(LIT, NorthstarOxygen.hasOxygen(context.getLevel(), context.getClickedPos()));
     }
 
-    @Inject(method = "updateShape", at = @At("TAIL"), cancellable = true)
+    @Inject(method = "updateShape", at = @At("RETURN"), cancellable = true)
     public void northstar$updateLit(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level,
                                     BlockPos pos, BlockPos neighborPos, CallbackInfoReturnable<BlockState> info) {
-        if (level instanceof Level l && !NorthstarOxygen.hasOxygen(l, pos) && state.getValue(LIT)) {
+        BlockState returned = info.getReturnValue() == null ? defaultBlockState() : info.getReturnValue();
+        if (returned.hasProperty(LIT) && returned.getValue(LIT) && level instanceof Level l && !NorthstarOxygen.hasOxygen(l, pos)) {
             level.playSound(null, pos, SoundEvents.CANDLE_EXTINGUISH, SoundSource.BLOCKS, 1, 0);
-            info.setReturnValue(state.setValue(LIT, false));
+            info.setReturnValue(returned.setValue(LIT, false));
         }
     }
 
