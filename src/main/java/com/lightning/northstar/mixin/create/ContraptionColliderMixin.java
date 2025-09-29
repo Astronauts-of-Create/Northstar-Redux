f984f20d81247077dabd5cff54e09e395bed5f28
Redstonneur1256 <redstonneur1256mc@gmail.com>
2025-09-29 15:48:40 +0200

    Do remap mixins

--- src/main/java/com/lightning/northstar/mixin/create/ContraptionColliderMixin.java
+++ src/main/java/com/lightning/northstar/mixin/create/ContraptionColliderMixin.java
@@ -9,14 +9,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = ContraptionCollider.class, remap = false)
+@Mixin(value = ContraptionCollider.class)
 public class ContraptionColliderMixin {
 
     @Redirect(method = "collideEntities",
             at = @At(value = "INVOKE",
                     target = "Lnet/minecraft/world/phys/Vec3;subtract(Lnet/minecraft/world/phys/Vec3;)Lnet/minecraft/world/phys/Vec3;",
-                    ordinal = 0,
-                    remap = false))
+                    ordinal = 0))
     private static Vec3 northstar$changeBounds(Vec3 instance, Vec3 vec, @Local(argsOnly = true) AbstractContraptionEntity entity) {
         if (entity instanceof RocketContraptionEntity) {
             return Vec3.ZERO; // pretend that we haven't moved, entities are moved manually in RocketContraptionEntity
