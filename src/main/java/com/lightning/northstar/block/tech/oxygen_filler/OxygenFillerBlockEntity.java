a41f3ee69184401111ef75af2d281930965f9395
Redstonneur1256 <redstonneur1256mc@gmail.com>
2025-10-24 16:53:41 +0200

    Oxygen Filler cleanup/rework

--- src/main/java/com/lightning/northstar/block/tech/oxygen_filler/OxygenFillerBlockEntity.java
+++ src/main/java/com/lightning/northstar/block/tech/oxygen_filler/OxygenFillerBlockEntity.java
@@ -1,190 +1,205 @@
 package com.lightning.northstar.block.tech.oxygen_filler;
 
-import com.lightning.northstar.content.NorthstarSounds;
+import com.lightning.northstar.content.NorthstarFluids;
 import com.lightning.northstar.content.NorthstarTags.NorthstarItemTags;
+import com.lightning.northstar.util.NorthstarLang;
 import com.lightning.northstar.world.oxygen.NorthstarOxygen;
 import com.simibubi.create.AllSoundEvents;
 import com.simibubi.create.api.equipment.goggles.IHaveGoggleInformation;
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
 import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
-import com.simibubi.create.foundation.blockEntity.behaviour.fluid.SmartFluidTankBehaviour;
-import com.simibubi.create.foundation.particle.AirParticleData;
+import com.simibubi.create.foundation.item.ItemHelper;
 import com.simibubi.create.foundation.utility.CreateLang;
-import net.createmod.catnip.lang.LangBuilder;
-import net.createmod.catnip.math.VecHelper;
 import net.minecraft.ChatFormatting;
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.capability.IFluidHandler.FluidAction;
-import net.minecraftforge.items.IItemHandlerModifiable;
-import net.minecraftforge.items.ItemStackHandler;
-import net.minecraftforge.items.wrapper.CombinedInvWrapper;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.items.wrapper.InvWrapper;
+import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
 public class OxygenFillerBlockEntity extends SmartBlockEntity implements IHaveGoggleInformation {
 
-    public int airLevel;
-    public int airTimer;
-    private boolean hasStopped = false;
-    private int audioTick = 0;
-
-    protected LazyOptional<IItemHandlerModifiable> itemCapability;
-    SmartFluidTankBehaviour tank;
-    public Container container = new SimpleContainer(1) {
+    public final Container container = new SimpleContainer(1);
+    public final IFluidHandler fluidHandler = new IFluidHandler() {
         @Override
-        public void setChanged() {
-            super.setChanged();
-            //             OxygenFillerBlockEntity.this.slotsChanged(this);
+        public int getTanks() {
+            return 1;
+        }
+
+        @Override
+        public FluidStack getFluidInTank(int tank) {
+            ItemStack item = getContainedItem();
+            if (tank == 0 || item == null || item.getTag() == null)
+                return FluidStack.EMPTY;
+            int oxygen = item.getTag().getInt("Oxygen");
+            return oxygen == 0 ? FluidStack.EMPTY : new FluidStack(NorthstarFluids.OXYGEN.getSource(), oxygen);
+        }
+
+        @Override
+        public int getTankCapacity(int tank) {
+            return getContainedItem() == null ? 0 : NorthstarOxygen.MAXIMUM_OXYGEN;
+        }
+
+        @Override
+        public boolean isFluidValid(int tank, FluidStack stack) {
+            return tank == 0 && NorthstarOxygen.isOxygen(stack.getFluid());
+        }
+
+        @Override
+        public int fill(FluidStack stack, FluidAction action) {
+            ItemStack item = getContainedItem();
+            if (!isFluidValid(0, stack) || item == null)
+                return 0;
+            CompoundTag tag = item.getOrCreateTag();
+            int oxygen = tag.getInt("Oxygen");
+            int fillable = Mth.clamp(NorthstarOxygen.MAXIMUM_OXYGEN - oxygen, 0, stack.getAmount());
+            if (action.execute() && fillable != 0) {
+                tag.putInt("Oxygen", oxygen + fillable);
+                sendData();
+                if (oxygen + fillable >= NorthstarOxygen.MAXIMUM_OXYGEN) {
+                    AllSoundEvents.CONFIRM.playOnServer(level, worldPosition, 0.4f, 0);
+                }
+            }
+            return fillable;
+        }
+
+        @Override
+        public FluidStack drain(FluidStack stack, FluidAction action) {
+            if (!isFluidValid(0, stack))
+                return FluidStack.EMPTY;
+            FluidStack drained = stack.copy();
+            drained.setAmount(drainAmount(stack.getAmount(), action));
+            return drained;
+        }
+
+        @Override
+        public FluidStack drain(int maxDrain, FluidAction action) {
+            return new FluidStack(NorthstarFluids.OXYGEN.getSource(), drainAmount(maxDrain, action));
+        }
+
+        private int drainAmount(int amount, FluidAction action) {
+            ItemStack item = getContainedItem();
+            if (item == null)
+                return 0;
+            CompoundTag tag = item.getOrCreateTag();
+            int drainable = Math.min(amount, tag.getInt("Oxygen"));
+            if (action.execute() && drainable != 0) {
+                tag.putInt("Oxygen", tag.getInt("Oxygen") - drainable);
+                sendData();
+            }
+            return drainable;
         }
     };
-    protected ItemStackHandler inventory;
 
     public OxygenFillerBlockEntity(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
         super(typeIn, pos, state);
-        inventory = new ItemStackHandler();
-        itemCapability = LazyOptional.of(() -> new CombinedInvWrapper(inventory));
     }
-
 
     @Override
     public void addBehaviours(List<BlockEntityBehaviour> behaviours) {
-        tank = SmartFluidTankBehaviour.single(this, 1000);
-        behaviours.add(tank);
     }
 
-    public ItemStackHandler getInventoryOfBlock() {
-        return inventory;
+    @Override
+    public void destroy() {
+        super.destroy();
+
+        ItemHelper.dropContents(level, worldPosition, new InvWrapper(container));
+        container.clearContent();
+    }
+
+    @Nullable
+    protected ItemStack getContainedItem() {
+        ItemStack item = container.getItem(0);
+        return NorthstarItemTags.OXYGEN_SOURCES.matches(item) ? item : null;
     }
 
     @Override
     public void tick() {
         super.tick();
-        ItemStack item = container.getItem(0);
-        Fluid fluid = tank.getPrimaryHandler().getFluid().getFluid();
-        int increment = 2;
-        if (item.is(NorthstarItemTags.OXYGEN_SOURCES.tag) && NorthstarOxygen.isOxygen(fluid)) {
-            CompoundTag tag = item.getOrCreateTag();
-            int currentOxy = tag.getInt("Oxygen");
-            while (currentOxy + increment > NorthstarOxygen.MAXIMUM_OXYGEN) {
-                increment--;
+
+        /*if (level.isClientSide) {
+            if (audioTick++ % 13 == 0) {
+                level.playLocalSound(worldPosition.getX(), worldPosition.getY(), worldPosition.getZ(), NorthstarSounds.AIRFLOW.get(), SoundSource.BLOCKS, 0.5f, 0, false);
             }
-            increment = Math.max(increment, 0);
-            if (increment == 0 && !hasStopped) {
-                AllSoundEvents.CONFIRM.playAt(level, worldPosition, 0.4f, 0, true);
-                hasStopped = true;
-            } else if (increment != 0) {
-                hasStopped = false;
-                audioTick++;
-                if (level.isClientSide) {
-                    if (audioTick % 13 == 0) {
-                        BlockPos pos = this.getBlockPos();
-                        level.playLocalSound(pos.getX(), pos.getY(), pos.getZ(), NorthstarSounds.AIRFLOW.get(), SoundSource.BLOCKS, 0.5f, 0, false);
-                    }
-                    Vec3 centerOf = VecHelper.getCenterOf(worldPosition);
-                    Vec3 v = VecHelper.offsetRandomly(centerOf, level.random, .65f);
-                    Vec3 m = centerOf.subtract(v);
-                    if (level.random.nextBoolean())
-                        level.addParticle(new AirParticleData(1, .05f), v.x, v.y, v.z, m.x, m.y, m.z);
-                }
+
+            if (level.random.nextBoolean()) {
+                Vec3 c = VecHelper.getCenterOf(worldPosition);
+                Vec3 v = VecHelper.offsetRandomly(c, level.random, .65f);
+                Vec3 m = c.subtract(v);
+                level.addParticle(new AirParticleData(1, .05f), v.x, v.y, v.z, m.x, m.y, m.z);
             }
-            int newOxyAmount = Mth.clamp(increment, 0, tank.getPrimaryHandler().getFluidAmount());
-            int newoxy = currentOxy + newOxyAmount;
-            tag.putInt("Oxygen", newoxy);
-            item.setTag(tag);
-            this.tank.getPrimaryHandler().drain(new FluidStack(fluid, newOxyAmount), FluidAction.EXECUTE);
-        }
+        }*/
     }
 
 
     @Override
     protected void write(CompoundTag compound, boolean clientPacket) {
         super.write(compound, clientPacket);
-        compound.putInt("Air", airLevel);
-        compound.putInt("Timer", airTimer);
-        compound.put("item", this.container.getItem(0).save(new CompoundTag()));
-    }
 
-    @Override
-    public void writeSafe(CompoundTag compound) {
-        super.writeSafe(compound);
-        compound.put("item", this.container.getItem(0).save(new CompoundTag()));
+        compound.put("item", container.getItem(0).save(new CompoundTag()));
     }
 
     @Override
     protected void read(CompoundTag compound, boolean clientPacket) {
         super.read(compound, clientPacket);
-        airLevel = compound.getInt("Air");
-        airTimer = compound.getInt("Timer");
-        this.container.setItem(0, ItemStack.of(compound.getCompound("item")));
+
+        container.setItem(0, ItemStack.of(compound.getCompound("item")));
     }
 
     @Override
     public boolean addToGoggleTooltip(List<Component> tooltip, boolean isPlayerSneaking) {
-        LangBuilder mb = CreateLang.translate("generic.unit.millibuckets");
-        CreateLang.translate("gui.goggles.oxygen_filler")
+        ItemStack item = container.getItem(0);
+        if (item.isEmpty())
+            return false;
+
+        NorthstarLang.translate("gui.goggles.oxygen_filler")
                 .forGoggles(tooltip);
-        FluidStack fluidStack = tank.getPrimaryHandler().getFluidInTank(0);
-        if (!fluidStack.getFluid().getFluidType().isAir()) {
-            CreateLang.fluidName(fluidStack)
-                    .style(ChatFormatting.GRAY)
-                    .forGoggles(tooltip);
-        } else {
-            CreateLang.translate("gui.goggles.empty")
-                    .style(ChatFormatting.GRAY)
-                    .forGoggles(tooltip);
-        }
-        CreateLang.builder()
-                .add(CreateLang.number(fluidStack.getAmount())
-                        .add(mb)
-                        .style(ChatFormatting.GOLD))
-                .text(ChatFormatting.GRAY, " / ")
-                .add(CreateLang.number(tank.getPrimaryHandler().getTankCapacity(0))
-                        .add(mb)
-                        .style(ChatFormatting.DARK_GRAY))
-                .forGoggles(tooltip, 1);
-        ItemStack itemStack = container.getItem(0);
-        if (!itemStack.isEmpty()) {
+        CreateLang.itemName(item)
+                .style(ChatFormatting.GRAY)
+                .forGoggles(tooltip);
+
+        if (NorthstarItemTags.OXYGEN_SOURCES.matches(item)) {
+            CompoundTag tag = item.getTag();
             CreateLang.builder()
-                    .add(CreateLang.number(itemStack.getCount())
-                            .style(ChatFormatting.GRAY))
-                    .text(ChatFormatting.DARK_GRAY, "x ")
-                    .add(CreateLang.itemName(itemStack)
-                            .style(ChatFormatting.GRAY))
+                    .add(CreateLang.number(tag != null ? tag.getInt("Oxygen") : 0)
+                            .add(NorthstarLang.MB)
+                            .style(ChatFormatting.GOLD))
+                    .text(ChatFormatting.GRAY, " / ")
+                    .add(CreateLang.number(NorthstarOxygen.MAXIMUM_OXYGEN)
+                            .add(NorthstarLang.MB)
+                            .style(ChatFormatting.DARK_GRAY))
                     .forGoggles(tooltip, 1);
-            CompoundTag thing = itemStack.getTag();
-            int currentOxy = thing.getInt("Oxygen");
-            if (currentOxy != 0) {
-                CreateLang.builder()
-                        .add(CreateLang.number(currentOxy)
-                                .style(ChatFormatting.GRAY))
-                        .add(mb)
-                        .style(ChatFormatting.GRAY)
-                        .forGoggles(tooltip, 2);
-            }
+        } else {
+            CreateLang.text("Cannot hold oxygen")
+                    .style(ChatFormatting.RED)
+                    .forGoggles(tooltip, 1);
         }
+
         return true;
     }
 
     @Override
-    public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
-        if (cap == ForgeCapabilities.FLUID_HANDLER && side == getBlockState().getValue(OxygenFillerBlock.HORIZONTAL_FACING).getOpposite())
-            return tank.getCapability().cast();
+    public <T> LazyOptional<T> getCapability(Capability<T> cap, @Nullable Direction side) {
+        if (cap == ForgeCapabilities.FLUID_HANDLER && (side == null || side == getBlockState().getValue(OxygenFillerBlock.HORIZONTAL_FACING).getOpposite()))
+            return LazyOptional.of(() -> fluidHandler).cast();
         return super.getCapability(cap, side);
     }
 
--- src/main/java/com/lightning/northstar/util/NorthstarLang.java
+++ src/main/java/com/lightning/northstar/util/NorthstarLang.java
@@ -1,8 +1,14 @@
 package com.lightning.northstar.util;
 
 import com.lightning.northstar.Northstar;
+import com.simibubi.create.foundation.fluid.SmartFluidTank;
 import com.simibubi.create.foundation.utility.CreateLang;
 import net.createmod.catnip.lang.LangBuilder;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.fluids.FluidStack;
+
+import java.util.List;
 
 public class NorthstarLang {
 
@@ -15,6 +21,31 @@ public class NorthstarLang {
 
     public static LangBuilder translate(String langKey, Object... args) {
         return builder().translate(langKey, args);
+    }
+
+    public static void addTankTooltip(List<Component> tooltip, SmartFluidTank tank) {
+        addTankTooltip(tooltip, tank.getFluid(), tank.getCapacity());
+    }
+
+    public static void addTankTooltip(List<Component> tooltip, FluidStack fluid, int capacity) {
+        if (!fluid.isEmpty()) {
+            CreateLang.fluidName(fluid)
+                    .style(ChatFormatting.GRAY)
+                    .forGoggles(tooltip);
+        } else {
+            CreateLang.translate("gui.goggles.empty")
+                    .style(ChatFormatting.GRAY)
+                    .forGoggles(tooltip);
+        }
+        CreateLang.builder()
+                .add(CreateLang.number(fluid.getAmount())
+                        .add(NorthstarLang.MB)
+                        .style(ChatFormatting.GOLD))
+                .text(ChatFormatting.GRAY, " / ")
+                .add(CreateLang.number(capacity)
+                        .add(NorthstarLang.MB)
+                        .style(ChatFormatting.DARK_GRAY))
+                .forGoggles(tooltip, 1);
     }
 
 }
