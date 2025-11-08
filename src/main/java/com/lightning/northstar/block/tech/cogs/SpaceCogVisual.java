02787b247cf11bd1e191570c0e9e2779bd9caa6a
Redstonneur1256 <redstonneur1256mc@gmail.com>
2025-11-08 17:10:46 +0100

    Fix iron cogwheel model, closes #53

--- src/main/java/com/lightning/northstar/block/tech/cogs/SpaceCogVisual.java
+++ src/main/java/com/lightning/northstar/block/tech/cogs/SpaceCogVisual.java
@@ -6,29 +6,14 @@ import com.jozufozu.flywheel.core.PartialModel;
 import com.simibubi.create.content.kinetics.base.ShaftInstance;
 import com.simibubi.create.content.kinetics.base.flwdata.RotatingData;
 import com.simibubi.create.content.kinetics.simpleRelays.BracketedKineticBlockEntity;
-import com.simibubi.create.content.kinetics.simpleRelays.BracketedKineticBlockEntityRenderer;
-import com.simibubi.create.content.kinetics.simpleRelays.ICogWheel;
 
 public record SpaceCogVisual(PartialModel small, PartialModel big) {
 
     public ShaftInstance<BracketedKineticBlockEntity> create(MaterialManager materialManager, BracketedKineticBlockEntity blockEntity) {
-        if (ICogWheel.isLargeCog(blockEntity.getBlockState())) {
-            return new ShaftInstance<>(materialManager, blockEntity) {
-                @Override
-                public void update() {
-                    rotatingModel.setRotationOffset(BracketedKineticBlockEntityRenderer.getShaftAngleOffset(axis, pos));
-                    super.update();
-                }
-                @Override
-                protected Instancer<RotatingData> getModel() {
-                    return getRotatingMaterial().getModel(big);
-                }
-            };
-        }
         return new ShaftInstance<>(materialManager, blockEntity) {
             @Override
             protected Instancer<RotatingData> getModel() {
-                return getRotatingMaterial().getModel(small);
+                return getRotatingMaterial().getModel(blockEntity.getBlockState());
             }
         };
     }
