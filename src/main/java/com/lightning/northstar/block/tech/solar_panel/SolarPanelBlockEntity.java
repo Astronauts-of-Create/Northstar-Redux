3e89d6499368839429475e1b9e7d71046b5ed599
Redstonneur1256 <redstonneur1256mc@gmail.com>
2025-11-21 12:08:35 +0100

    Fix solar panels breaking for good

--- src/main/java/com/lightning/northstar/block/tech/solar_panel/SolarPanelBlockEntity.java
+++ src/main/java/com/lightning/northstar/block/tech/solar_panel/SolarPanelBlockEntity.java
@@ -18,7 +18,6 @@ public class SolarPanelBlockEntity extends GeneratingKineticBlockEntity {
 
     private int lastLight = -1;
     private float generatedSpeed;
-    private int ticksSinceChange;
 
     public SolarPanelBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
@@ -29,10 +28,9 @@ public class SolarPanelBlockEntity extends GeneratingKineticBlockEntity {
         super.tick();
 
         int light = Math.max(0, level.getBrightness(LightLayer.SKY, worldPosition) - level.getSkyDarken());
-        if (light != lastLight && ticksSinceChange++ >= 40) {
+        if (light != lastLight && getFlickerScore() <= 64) {
             lastLight = light;
             generatedSpeed = MAXIMUM_SPEED * light / 15f * NorthstarPlanets.getSunMultiplier(level.dimension());
-            ticksSinceChange = 0;
             updateGeneratedRotation();
         }
 
@@ -46,7 +44,6 @@ public class SolarPanelBlockEntity extends GeneratingKineticBlockEntity {
         super.read(compound, clientPacket);
         generatedSpeed = compound.getFloat("GeneratorSpeed");
         lastLight = -1;
-        ticksSinceChange = 0;
     }
 
     @Override
