c06d5d10402a9254dd0a181d203511ffa1beeb16
Redstonneur1256 <redstonneur1256mc@gmail.com>
2025-11-29 17:07:43 +0100

    Change default freezing/boiling temperatures, closes #75

--- src/main/java/com/lightning/northstar/world/temperature/NorthstarTemperature.java
+++ src/main/java/com/lightning/northstar/world/temperature/NorthstarTemperature.java
@@ -184,7 +184,7 @@ public class NorthstarTemperature {
             return -253;
         if (state.is(NorthstarFluids.SULFURIC_ACID.get()) || state.is(NorthstarFluids.SULFURIC_ACID.getSource().getSource()))
             return 1200;
-        return 100;
+        return Integer.MAX_VALUE;
     }
 
     public static int getFreezingPoint(FluidState state) {
@@ -201,7 +201,7 @@ public class NorthstarTemperature {
             return -259;
         if (state.is(NorthstarFluids.SULFURIC_ACID.get()) || state.is(NorthstarFluids.SULFURIC_ACID.getSource().getSource()))
             return -200;
-        return 0;
+        return Integer.MIN_VALUE;
     }
 
     public static boolean hasInsulation(LivingEntity entity) {
