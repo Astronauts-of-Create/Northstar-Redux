66e3c913972f7add80807625312c8429ad9c3a9c
Redstonneur1256 <redstonneur1256mc@gmail.com>
2026-01-15 11:06:11 +0100

    Fix sequenced recipe items not being hidden

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("dev.architectury.loom") version "1.11.440"
 }
 
-version = "0.5.3+1.20.1" // https://semver.org/
+version = "0.5.4+1.20.1" // https://semver.org/
 group = "com.lightning.northstar" // http://maven.apache.org/guides/mini/guide-naming-conventions.html
 
 java {
