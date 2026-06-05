dc0c35380105662991f7563ab382ff34d96afb85
Redstonneur1256 <redstonneur1256mc@gmail.com>
2026-06-05 16:23:31 +0200

    Remove Computer Rack tile entity ticker

--- src/main/java/com/lightning/northstar/block/tech/computer_rack/TargetingComputerRackBlock.java
+++ src/main/java/com/lightning/northstar/block/tech/computer_rack/TargetingComputerRackBlock.java
@@ -16,10 +16,13 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -81,6 +84,12 @@ public class TargetingComputerRackBlock extends HorizontalKineticBlock implement
     @Override
     public Axis getRotationAxis(BlockState state) {
         return Axis.Y;
+    }
+
+    @Override
+    @Nullable
+    public <S extends BlockEntity> BlockEntityTicker<S> getTicker(Level level, BlockState state, BlockEntityType<S> type) {
+        return null;
     }
 
     @Override
--- src/main/java/com/lightning/northstar/block/tech/computer_rack/TargetingComputerRackBlockEntity.java
+++ src/main/java/com/lightning/northstar/block/tech/computer_rack/TargetingComputerRackBlockEntity.java
@@ -1,6 +1,7 @@
 package com.lightning.northstar.block.tech.computer_rack;
 
 import com.lightning.northstar.content.NorthstarItems;
+import com.lightning.northstar.util.BetterSimpleContainer;
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
 import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
 import com.simibubi.create.foundation.item.ItemHelper;
@@ -8,7 +9,6 @@ import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
-import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -22,7 +22,7 @@ import java.util.List;
 @ParametersAreNonnullByDefault
 public class TargetingComputerRackBlockEntity extends SmartBlockEntity {
 
-    public final Container container = new SimpleContainer(6);
+    public final SimpleContainer container = new BetterSimpleContainer(6);
 
     public TargetingComputerRackBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
@@ -42,28 +42,26 @@ public class TargetingComputerRackBlockEntity extends SmartBlockEntity {
     @Override
     protected void read(CompoundTag compound, boolean clientPacket) {
         super.read(compound, clientPacket);
-        if (compound.contains("slot1", Tag.TAG_COMPOUND)) container.setItem(0, ItemStack.of(compound.getCompound("slot1")));
-        if (compound.contains("slot2", Tag.TAG_COMPOUND)) container.setItem(1, ItemStack.of(compound.getCompound("slot2")));
-        if (compound.contains("slot3", Tag.TAG_COMPOUND)) container.setItem(2, ItemStack.of(compound.getCompound("slot3")));
-        if (compound.contains("slot4", Tag.TAG_COMPOUND)) container.setItem(3, ItemStack.of(compound.getCompound("slot4")));
-        if (compound.contains("slot5", Tag.TAG_COMPOUND)) container.setItem(4, ItemStack.of(compound.getCompound("slot5")));
-        if (compound.contains("slot6", Tag.TAG_COMPOUND)) container.setItem(5, ItemStack.of(compound.getCompound("slot6")));
+
+        container.fromTag(compound.getList("Items", Tag.TAG_COMPOUND));
+
+        for (int i = 0; i < 6; i++) {
+            if (compound.contains("slot" + (i + 1), Tag.TAG_COMPOUND)) {
+                container.setItem(i, ItemStack.of(compound.getCompound("slot" + (i + 1))));
+            }
+        }
     }
 
     @Override
     public void write(CompoundTag compound, boolean clientPacket) {
         super.write(compound, clientPacket);
-        compound.put("slot1", container.getItem(0).serializeNBT());
-        compound.put("slot2", container.getItem(1).serializeNBT());
-        compound.put("slot3", container.getItem(2).serializeNBT());
-        compound.put("slot4", container.getItem(3).serializeNBT());
-        compound.put("slot5", container.getItem(4).serializeNBT());
-        compound.put("slot6", container.getItem(5).serializeNBT());
+
+        compound.put("Items", container.createTag());
     }
 
     public int getComputerCount() {
         int count = 0;
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < container.getContainerSize(); i++) {
             if (container.getItem(i).is(NorthstarItems.TARGETING_COMPUTER.get())) {
                 count++;
             }
