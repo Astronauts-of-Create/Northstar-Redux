ed314436c98dd0aa739816f5c5098b633eefaccd
Redstonneur1256 <redstonneur1256mc@gmail.com>
2026-09-12 11:07:41 +0200

    Check for windmill speed updates

--- src/main/java/com/lightning/northstar/mixin/block/WindmillBearingBlockEntityMixin.java
+++ src/main/java/com/lightning/northstar/mixin/block/WindmillBearingBlockEntityMixin.java
@@ -4,16 +4,56 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.simibubi.create.content.contraptions.bearing.MechanicalBearingBlockEntity;
 import com.simibubi.create.content.contraptions.bearing.WindmillBearingBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = WindmillBearingBlockEntity.class, remap = false)
 public abstract class WindmillBearingBlockEntityMixin extends MechanicalBearingBlockEntity {
 
+    @Shadow
+    public abstract void updateGeneratedRotation();
+
+    @Unique
+    private float northstar$windMultiplier;
+
     public WindmillBearingBlockEntityMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
+    }
+
+    @Inject(
+            method = "<init>",
+            at = @At("TAIL")
+    )
+    private void northstar$init(BlockEntityType<?> type, BlockPos pos, BlockState state, CallbackInfo ci) {
+        northstar$windMultiplier = 1;
+    }
+
+    @Inject(
+            method = "tick",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lcom/simibubi/create/content/contraptions/bearing/WindmillBearingBlockEntity;queuedReassembly:Z",
+                    opcode = Opcodes.GETFIELD,
+                    ordinal = 0
+            )
+    )
+    private void northstar$tick(CallbackInfo ci) {
+        float windMultiplier = level.northstar$dimension().wind().get(level, worldPosition);
+        if (!Mth.equal(windMultiplier, northstar$windMultiplier) && getFlickerScore() <= 64) {
+            northstar$windMultiplier = windMultiplier;
+            updateGeneratedRotation();
+            notifyUpdate();
+        }
     }
 
     @ModifyReturnValue(
@@ -24,7 +64,23 @@ public abstract class WindmillBearingBlockEntityMixin extends MechanicalBearingB
             )
     )
     private float northstar$updateGeneratedSpeed(float value) {
-        return value * level.northstar$dimension().wind().get(level, worldPosition);
+        return value * northstar$windMultiplier;
+    }
+
+    @Inject(
+            method = "write",
+            at = @At("TAIL")
+    )
+    private void northstar$write(CompoundTag compound, HolderLookup.Provider registries, boolean clientPacket, CallbackInfo ci) {
+        compound.putFloat("NorthstarWindMultiplier", northstar$windMultiplier);
+    }
+
+    @Inject(
+            method = "read",
+            at = @At("TAIL")
+    )
+    private void northstar$read(CompoundTag compound, HolderLookup.Provider registries, boolean clientPacket, CallbackInfo ci) {
+        northstar$windMultiplier = compound.getFloat("NorthstarWindMultiplier");
     }
 
 }
